e47b1932c941b2efd7c2f8b05dc97197ea8f2aee
NicoGlo <nicoglo2103@gmail.com>
2023-02-02 12:11:39 +0100

    Meho

--- Assets/MovilPC/Menu.cs
+++ Assets/MovilPC/Menu.cs
@@ -7,13 +7,23 @@ public class Menu : MonoBehaviour
 {
     public void NewGameClick()
     {
-        //Bot�n de iniciar juego, te manda a la escena del pantano
+        //Bot�n de iniciar juego, te manda a la escena del juego
         SceneManager.LoadScene("SampleScene");
     }
-
+    public void CreditosClick()
+    {
+        //Bot�n de iniciar créditos, te manda a la escena de los créditos
+        SceneManager.LoadScene("Creditos");
+    }
     public void ExitClick()
     {
         //Bot�n para salir del juego
         Application.Quit();
+    }
+
+    public void MenuClick()
+    {
+        //Bot�n de iniciar créditos, te manda a la escena de los créditos
+        SceneManager.LoadScene("Menu");
     }
 }
